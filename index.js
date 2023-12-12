aeb2ddda8accb6dd733a6a65c541ada01f8523a1
karaneliab <eliabkaran7884@gmail.com>
2023-12-12 20:02:00 +0300

    done with arithimetic

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+// Write your code
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+
+let num3 = 16;
+let num4 = 6;
+
+let mod = num3 %  num4;
+
+console.log(mod);
+
+let max = Math.max(15,17,10,20,9);
+console.log(max);
